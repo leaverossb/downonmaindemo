2804af3c763ae45c6d9d0ed8f00155b77d9360dd
Phillip Ross <42011858+leaverossb@users.noreply.github.com>
2026-09-25 18:37:14 -0400

    Update main.js

--- main.js
+++ main.js
@@ -18,8 +18,6 @@ document.addEventListener("DOMContentLoaded", () => {
     document.getElementById('performanceVidLink').src = videoLinks[randomLink];
 });
 
-<iframe width="560" height="315" src="" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-
 /*************
     BANDS
  *************/
